3a7140e1801b7e91b1b99663134d1d88d5db6f9b
agree0002 <163145323+agree0002@users.noreply.github.com>
2024-12-16 19:42:09 +0900

    Add files via upload

---
+++ domain/User.java
@@ -0,0 +1,13 @@
+package project.planner.domain;
+
+public class User {
+    private int index;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
